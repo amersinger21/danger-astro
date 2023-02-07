0a0926e92cc03605a76a5ecb9388740bcbb3e053
amersinger21 <amersinger21@verizon.net>
2023-02-06 19:23:25 -0500

    astro config update for music collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,34 @@
 import { defineConfig } from "astro/config";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        collections: [
+          {
+            name: "music",
+            label: "Music",
+            folder: "src/content/music",
+            slug: "{{sortOrder}}-{{slug}}",
+            create: true,
+            delete: true,
+            fields: [
+              { label: "Title", name: "title", widget: "string" },
+              { label: "Link", name: "link", widget: "string" },
+              { label: "Release Year", name: "year", widget: "string" },
+              { label: "Slug", name: "slug", widget: "string" },
+              { label: "Item Number", name: "sortOrder", widget: "number" },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
+});
