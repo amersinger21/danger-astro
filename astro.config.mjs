a89ccc7fbdc68bb2fe7028aa994abad8f3accf38
amersinger21 <amersinger21@verizon.net>
2023-02-06 19:38:49 -0500

    trying manually again for cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,5 @@
 import { defineConfig } from "astro/config";
-import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 
-export default defineConfig({
-  integrations: [
-    NetlifyCMS({
-      config: {
-        backend: {
-          name: "git-gateway",
-          branch: "main",
-        },
-        collections: [
-          {
-            name: "music",
-            label: "Music",
-            folder: "src/content/music",
-            slug: "{{sortOrder}}-{{slug}}",
-            create: true,
-            delete: true,
-            fields: [
-              { label: "Title", name: "title", widget: "string" },
-              { label: "Link", name: "link", widget: "string" },
-              { label: "Release Year", name: "year", widget: "string" },
-              { label: "Slug", name: "slug", widget: "string" },
-              { label: "Item Number", name: "sortOrder", widget: "number" },
-            ],
-          },
-        ],
-      },
-    }),
-  ],
-});
+export default defineConfig({});
